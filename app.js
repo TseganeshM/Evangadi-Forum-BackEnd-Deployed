2e29e26a3726e30b5ddc4221fe549b0b136be9d7
TseganeshM <mogatsega@gmail.com>
2024-11-18 16:50:37 +0300

    updates on code

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-require(`dotenv`).config();
 const express = require("express");
 const app = express();
+require(`dotenv`).config();
 const port = process.env.PORT || 5500;
-const cors = require("cors");
+const cors = require(`cors`);
 
 app.use(cors());
 
 // db connection
-//const dbConnection = require("./db/dbConfig");
+const dbConnection = require("./db/dbConfig");
+
+//user routes middleware
+const userRoutes = require("./routes/userRoute");
+//question routes middleware file
+const questionsRoute = require("./routes/questionRoute");
+//answer routes middleware file
+const answerRoute = require("./routes/answerRoute");
 
 //authentication middleware
 const authMiddleware = require("./middleware/authMiddleware");
 app.use(express.json());
 
 //user routes middleware
-const userRoutes = require("./routes/userRoute");
 app.use("/api/user", userRoutes);
-
 //question routes middleware
-const questionsRoute = require("./routes/questionRoute");
 app.use("/api/questions", authMiddleware, questionsRoute);
-
-//answer routes middleware file
-const answerRoute = require("./routes/answerRoute");
+//answer routes middleware
 app.use("/api/answer", authMiddleware, answerRoute);
 
 async function start() {
   try {
-    //const result = await dbConnection.execute("SELECT 'Test'");
+    const result = await dbConnection.execute("SELECT 'Test'");
+    console.log(result);
     await app.listen(port);
     console.log("database connection established");
     console.log(`listening on ${port}`);
